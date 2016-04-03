3bb9450af985ae9efc103b75845c303e6b753083
orivil <307509265@qq.com>
2016-04-03 13:39:28 +0800

    新文件:       comment.go 	新文件:       example_test.go

---
+++ comment.go
@@ -0,0 +1,48 @@
+package comment
+
+import (
+	"go/parser"
+	"go/token"
+	"os"
+	"go/doc"
+)
+
+type StructComment map[string]string		// {package.struct: comment}
+
+type MethodComment map[string]map[string]string	// {package.struct: {methodName: comment}}
+
+type FuncComment map[string]string		// {package.func: comment}
+
+func GetDirComment(filter func(f os.FileInfo) bool, dirs... string) (sc StructComment, mc MethodComment, fc FuncComment, e error) {
+
+	sc = make(StructComment, 2)
+	mc = make(MethodComment, 5)
+	fc = make(FuncComment, 2)
+	for _, dir := range dirs {
+		pkgs, err := parser.ParseDir(token.NewFileSet(), dir, filter, parser.ParseComments)
+		if err != nil {
+			e = err
+			return
+		}
+
+		for _, p := range pkgs {
+			doc := doc.New(p, dir, doc.AllDecls)
+
+			for _, f := range doc.Funcs {
+				fc[p.Name + "." + f.Name] = f.Doc
+			}
+
+			for _, t := range doc.Types {
+				pkgStruct := p.Name + "." + t.Name
+				sc[pkgStruct] = t.Doc
+				if len := len(t.Methods); len > 0 {
+					mc[pkgStruct] = make(map[string]string, len)
+				}
+				for _, m := range t.Methods {
+					mc[pkgStruct][m.Name] = m.Doc
+				}
+			}
+		}
+	}
+	return
+}
---
+++ example_test.go
@@ -0,0 +1,50 @@
+package comment_test
+
+import (
+	"os"
+	"fmt"
+	"github.com/orivil/comment"
+	"log"
+)
+
+
+// controller comment
+type controller struct {
+
+}
+
+// @route {get}/
+func (c *controller) index() {
+
+}
+
+// add two parameters
+func add(a, b int) int {
+	return a + b
+}
+
+func ExampleGetDirComment() {
+	fileFilter := func(f os.FileInfo) bool {
+		// match all file
+		return true
+	}
+
+	structComment, methodComment, functionComment, err := comment.GetDirComment(fileFilter, "./")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get struct comment
+	fmt.Println(structComment["comment_test.controller"] == "controller comment\n")
+
+	// get method comment
+	fmt.Println(methodComment["comment_test.controller"]["index"] == "@route {get}/\n")
+
+	// get function comment
+	fmt.Println(functionComment["comment_test.add"] == "add two parameters\n")
+
+	// Output:
+	// true
+	// true
+	// true
+}
